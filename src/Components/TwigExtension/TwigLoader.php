99025487e04362798c59af64de1cf962c12b0e21
Enjoys <deadly.info@gmail.com>
2021-03-23 14:26:39 +0300

    added twig loader for include absolute paths

---
+++ src/Components/TwigExtension/TwigLoader.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace EnjoysCMS\Core\Components\TwigExtension;
+
+
+use Twig\Loader\FilesystemLoader;
+
+class TwigLoader extends FilesystemLoader
+{
+    protected function findTemplate(string $name, bool $throw = true)
+    {
+        if (isset($this->cache[$name])) {
+            return $this->cache[$name];
+        }
+
+        if (is_file($name)) {
+            $this->cache[$name] = $name;
+            return $name;
+        }
+
+        return parent::findTemplate($name, $throw);
+    }
+}
