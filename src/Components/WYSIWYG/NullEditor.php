68a4fa838f5ddaf70abe7ce9aaef71503d5919e9
Enjoys <deadly.info@gmail.com>
2021-05-21 18:31:18 +0300

    refactor wysiwyg nulleditor

--- src/Components/WYSIWYG/NullEditor.php
+++ src/Components/WYSIWYG/NullEditor.php
@@ -8,9 +8,7 @@ namespace EnjoysCMS\Core\Components\WYSIWYG;
 
 final class NullEditor implements WysiwygInterface
 {
-    private string $twigTemplate = '@wysisyg/.gitkeep';
-
-    public function getTwigTemplate(){
-        return $this->twigTemplate;
+     public function getTwigTemplate(){
+        return __DIR__.'/nulleditor.twig';
     }
 }
