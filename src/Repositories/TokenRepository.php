e22e97998b5a23ae8f66666648bc7310b31d86cd
Enjoys <deadly.info@gmail.com>
2021-06-04 17:26:02 +0300

    added +clearAllInactiveTokens() added -gc()

--- src/Repositories/TokenRepository.php
+++ src/Repositories/TokenRepository.php
@@ -13,8 +13,16 @@ use EnjoysCMS\Core\Entities\Users;
 class TokenRepository extends EntityRepository
 {
 
+    /**
+     * gcProbability int 10 the probability (parts per million) that garbage collection (GC) should be performed
+     *      when storing a piece of data in the cache. Defaults to 10, meaning 0.001% chance.
+     *      This number should be between 0 and 1000000. A value 0 means no GC will be performed at all.
+     */
+    private int $gcProbability = 10;
+
     public function clearInactiveTokensByUser(Users $user)
     {
+        $this->gc();
 
         $qb = $this->createQueryBuilder('t')
             ->delete(Token::class, 't')
@@ -24,5 +32,26 @@ class TokenRepository extends EntityRepository
             ->setParameter('now', new \DateTimeImmutable())
         ;
         $qb->getQuery()->getResult();
+    }
+
+
+    public function clearAllInactiveTokens()
+    {
+        $qb = $this->createQueryBuilder('t')
+            ->delete(Token::class, 't')
+            ->andWhere('t.exp < :now')
+            ->setParameter('now', new \DateTimeImmutable())
+        ;
+        $qb->getQuery()->getResult();
+    }
+
+    /**
+     * @throws \Exception
+     */
+    private function gc()
+    {
+        if (\random_int(0, 1000000) < $this->gcProbability) {
+            $this->clearAllInactiveTokens();
+        }
     }
 }
