95a4369e5048bfcef56d60a7153989ed3683fa6a
Enjoys <deadly.info@gmail.com>
2021-06-11 13:11:04 +0300

    added logger to helper

--- src/Components/Helpers/Setting.php
+++ src/Components/Helpers/Setting.php
@@ -4,15 +4,21 @@
 namespace EnjoysCMS\Core\Components\Helpers;
 
 
+use Psr\Log\LoggerInterface;
+
 class Setting extends HelpersBase
 {
     public static function get(string $key, $default = null)
     {
         $settings = self::$container->get('Setting');
+        /** @var LoggerInterface $logger */
+        $logger = self::$container->get(LoggerInterface::class)->withName('Setting');
 
         if (array_key_exists($key, $settings)) {
             return $settings[$key];
         }
+
+        $logger->debug(sprintf('Parameter `%s` not found! Return default value', $key));
 
         return $default;
     }
