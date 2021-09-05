1639c658be4682c4061669249e306e113dd38722
enjoys <deadly.info@gmail.com>
2021-09-05 21:28:45 +0300

    added twig function `regex_replace`
    {{ var|regex_replace(pattern, replacement) }}

--- src/Components/TwigExtension/ExtendedFunctions.php
+++ src/Components/TwigExtension/ExtendedFunctions.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace EnjoysCMS\Core\Components\TwigExtension;
 
 use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
 use Twig\TwigTest;
 
 final class ExtendedFunctions extends AbstractExtension
@@ -14,6 +16,18 @@ final class ExtendedFunctions extends AbstractExtension
         return array(
             new TwigTest('instanceof', array($this, 'isInstanceOf'))
         );
+    }
+
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('regex_replace', [$this, 'regexReplace'], ['is_safe' => ['all']])
+        ];
+    }
+
+    public function regexReplace(?string $body, string $pattern, string $replacement)
+    {
+        return preg_replace($pattern, $replacement, $body);
     }
 
     public function isInstanceOf($object, $class)
