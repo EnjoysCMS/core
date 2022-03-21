cd720d2031d0938c3a5763519e910e38e8c10028
Enjoys <deadly.info@gmail.com>
2022-03-21 18:58:25 +0300

    refactor to use Psr\Http\Message\ServerRequestInterface with Attributes

--- src/BaseController.php
+++ src/BaseController.php
@@ -6,14 +6,26 @@ declare(strict_types=1);
 namespace EnjoysCMS\Core;
 
 
+use HttpSoft\Message\Response;
+use HttpSoft\ServerRequest\ServerRequestCreator;
+use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\StreamInterface;
 
 abstract class BaseController
 {
-    public function __construct(protected ResponseInterface $response)
+    protected ServerRequestInterface $request;
+    protected ResponseInterface $response;
+
+    public function __construct(ResponseInterface $response = null, ServerRequestInterface $request = null)
     {
+        $this->response = $response ?? new Response();
+        $this->request = $request ?? ServerRequestCreator::createFromGlobals();
+
     }
+
+
 
     protected function responseText(string $body = ''): ResponseInterface
     {
