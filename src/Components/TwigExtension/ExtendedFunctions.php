8e52830a41b5a437e2168442ab31ae0f2dc3f653
Enjoys <deadly.info@gmail.com>
2022-05-20 14:55:59 +0300

    added twig filters: base64_encode and base64_decode

--- src/Components/TwigExtension/ExtendedFunctions.php
+++ src/Components/TwigExtension/ExtendedFunctions.php
@@ -21,8 +21,20 @@ final class ExtendedFunctions extends AbstractExtension
     public function getFilters(): array
     {
         return [
-            new TwigFilter('regex_replace', [$this, 'regexReplace'], ['is_safe' => ['all']])
+            new TwigFilter('regex_replace', [$this, 'regexReplace'], ['is_safe' => ['all']]),
+            new TwigFilter('base64_decode', [$this, 'base64Decode'], ['is_safe' => ['all']]),
+            new TwigFilter('base64_encode', [$this, 'base64Encode'], ['is_safe' => ['all']]),
         ];
+    }
+
+    public function base64Decode(string $body, bool $strict = false)
+    {
+        return \base64_decode($body, $strict);
+    }
+
+    public function base64Encode(string $body)
+    {
+        return \base64_encode($body);
     }
 
     public function regexReplace(?string $body, string $pattern, string $replacement)
