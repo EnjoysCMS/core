39f94a4d6fd517ec4c658cabd8016017dd8cf362
Enjoys <deadly.info@gmail.com>
2022-05-26 11:14:33 +0300

    fix NullEditor.php for new interface

--- src/Components/WYSIWYG/NullEditor.php
+++ src/Components/WYSIWYG/NullEditor.php
@@ -8,8 +8,12 @@ namespace EnjoysCMS\Core\Components\WYSIWYG;
 
 final class NullEditor implements WysiwygInterface
 {
-     public function getTwigTemplate(): string
-     {
-        return __DIR__.'/nulleditor.twig';
+    public function getTwigTemplate(): string
+    {
+        return __DIR__ . '/nulleditor.twig';
+    }
+
+    public function setTwigTemplate(?string $twigTemplate): void
+    {
     }
 }
