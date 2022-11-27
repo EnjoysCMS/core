9bf95e34a1e5a269fe814b62d3be419d11526c1c
enjoys <deadly.info@gmail.com>
2022-11-27 14:25:56 +0300

    remember param added

--- src/Components/Auth/Strategy/PhpSession.php
+++ src/Components/Auth/Strategy/PhpSession.php
@@ -48,7 +48,9 @@ final class PhpSession implements StrategyInterface
             ]
         );
 
-        $this->writeToken($user, $data['token'] ?? null);
+        if ($data['remember'] ?? true){
+            $this->writeToken($user, $data['token'] ?? null);
+        }
     }
 
     /**
