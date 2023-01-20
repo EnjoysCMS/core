78f82d05df9a247e4bdbc4fe511269189fb5df6f
enjoys <deadly.info@gmail.com>
2023-01-20 21:30:59 +0300

    added test testLogout(), testDeleteTokenIfExist(), testDeleteTokenIfNotExist()

--- tests/Components/Auth/Strategy/PhpSessionTest.php
+++ tests/Components/Auth/Strategy/PhpSessionTest.php
@@ -6,9 +6,11 @@ namespace Tests\EnjoysCMS\Components\Auth\Strategy;
 
 use Doctrine\ORM\EntityManager;
 use Enjoys\Cookie\Cookie;
+use Enjoys\Cookie\Options;
 use Enjoys\Session\Session;
 use EnjoysCMS\Core\Components\Auth\Strategy\PhpSession;
 use EnjoysCMS\Core\Components\Helpers\Config;
+use EnjoysCMS\Core\Entities\Token;
 use EnjoysCMS\Core\Entities\User;
 use EnjoysCMS\Core\Repositories\TokenRepository;
 use PHPUnit\Framework\TestCase;
@@ -63,5 +65,53 @@ class PhpSessionTest extends TestCase
         $authStrategy = new PhpSession($em, $session, $cookie);
 
         $authStrategy->login(new User(), ['remember' => true]);
+    }
+
+    public function testLogout()
+    {
+        $em = $this->getMock(EntityManager::class);
+        $session = $this->createMock(Session::class);
+        $authStrategy = new PhpSession($em, $session, new Cookie(new Options()));
+        $session->expects($this->exactly(2))->method('delete');
+
+        $authStrategy->logout();
+    }
+
+    public function testDeleteTokenIfExist()
+    {
+        $em = $this->getMock(EntityManager::class);
+        $session = $this->createMock(Session::class);
+        $cookie = $this->getMock(Cookie::class);
+        $tokenRepository = $this->createMock(TokenRepository::class);
+        $tokenRepository->method('find')->willReturn(new Token());
+        $em->method('getRepository')->willReturn($tokenRepository);
+        $container = $this->getMock(ContainerInterface::class);
+        $config = $this->getMock(\Enjoys\Config\Config::class);
+        $container->method('get')->willReturn($config);
+        Config::setContainer($container);
+
+        $authStrategy = new PhpSession($em, $session, $cookie);
+        $em->expects($this->exactly(1))->method('remove');
+        $em->expects($this->exactly(1))->method('flush');
+        $authStrategy->deleteToken('token');
+    }
+
+    public function testDeleteTokenIfNotExist()
+    {
+        $em = $this->getMock(EntityManager::class);
+        $session = $this->createMock(Session::class);
+        $cookie = $this->getMock(Cookie::class);
+        $tokenRepository = $this->createMock(TokenRepository::class);
+        $tokenRepository->method('find')->willReturn(null);
+        $em->method('getRepository')->willReturn($tokenRepository);
+        $container = $this->getMock(ContainerInterface::class);
+        $config = $this->getMock(\Enjoys\Config\Config::class);
+        $container->method('get')->willReturn($config);
+        Config::setContainer($container);
+
+        $authStrategy = new PhpSession($em, $session, $cookie);
+        $em->expects($this->exactly(0))->method('remove');
+        $em->expects($this->exactly(0))->method('flush');
+        $authStrategy->deleteToken('token');
     }
 }
