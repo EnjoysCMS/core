1d2a2b6e1e7430389756ca59216c01c482d8938c
enjoys <deadly.info@gmail.com>
2023-01-27 09:40:29 +0300

    added middlewares

---
+++ src/Middleware/AclMiddleware.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace EnjoysCMS\Core\Middleware;
+
+
+use Doctrine\ORM\Exception\ORMException;
+use Doctrine\ORM\OptimisticLockException;
+use Enjoys\Config\Config;
+use EnjoysCMS\Core\Components\AccessControl\ACL;
+use EnjoysCMS\Core\Exception\ForbiddenException;
+use InvalidArgumentException;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Symfony\Component\Routing\RouteCollection;
+
+final class AclMiddleware implements MiddlewareInterface
+{
+    public function __construct(
+        private RouteCollection $routeCollection,
+        private Config $config,
+        private ACL $acl
+    ) {
+    }
+
+    /**
+     * @throws ForbiddenException
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $route = $this->routeCollection->get($request->getAttribute('_route', ''));
+        if ($route === null) {
+            throw new InvalidArgumentException('Route not set');
+        }
+        $controller = implode('::', (array)$route->getDefault('_controller'));
+
+        if ($route->getOption('acl') !== false
+            && !$this->acl->access($controller, (string)($route->getOption('comment') ?? $controller))
+            && !$this->config->get('acl->disableChecking', false)
+        ) {
+            throw new ForbiddenException();
+        }
+        return $handler->handle($request);
+    }
+}
