d9fc7a82813f570f6f2c71bc57b596d5e254de73
enjoys <deadly.info@gmail.com>
2023-08-11 08:30:51 +0300

    added \EnjoysCMS\Core\Extensions\Doctrine\Subscribers\SqlitePreFlushSubscriber

---
+++ src/Extensions/Doctrine/Subscribers/SqlitePreFlushSubscriber.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace EnjoysCMS\Core\Extensions\Doctrine\Subscribers;
+
+use Doctrine\Common\EventSubscriber;
+use Doctrine\DBAL\Exception;
+use Doctrine\DBAL\Platforms\SqlitePlatform;
+use Doctrine\ORM\Event\PreFlushEventArgs;
+use Doctrine\ORM\Events;
+
+/**
+ * Foreign key checks are disabled by default on pdo_sqlite driver.
+ *
+ * As it's mentioned here:
+ *
+ * Foreign key constraints are disabled by default (for backwards compatibility),
+ * so must be enabled separately for each database connection. (Note, however,
+ * that future releases of SQLite might change so that foreign key constraints
+ * enabled by default. Careful developers will not make any assumptions about
+ * whether foreign keys are enabled by default but will instead enable
+ * or disable them as necessary.
+ *
+ * $evm = new EventManager();
+ * $evm->addEventSubscriber(new SqlitePreFlushSubscriber());
+ */
+final class SqlitePreFlushSubscriber implements EventSubscriber
+{
+    public function getSubscribedEvents(): array
+    {
+        return [Events::preFlush];
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function preFlush(PreFlushEventArgs $args): void
+    {
+        $connection = $args->getObjectManager()->getConnection();
+
+        if ($connection->getDatabasePlatform() instanceof SqlitePlatform) {
+            $connection->executeStatement('PRAGMA foreign_keys = ON;');
+        }
+    }
+
+}
