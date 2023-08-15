bbe3e0faa0afecb6ef974a99bde45a7f582dfc00
enjoys <deadly.info@gmail.com>
2023-08-15 08:52:51 +0300

    added `users event`

---
+++ src/Users/Events/AfterDeleteUserEvent.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EnjoysCMS\Core\Users\Events;
+
+use Symfony\Contracts\EventDispatcher\Event;
+
+final class AfterDeleteUserEvent extends Event
+{
+
+}
---
+++ src/Users/Events/BeforeDeleteUserEvent.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace EnjoysCMS\Core\Users\Events;
+
+use EnjoysCMS\Core\Users\Entity\User;
+use Symfony\Contracts\EventDispatcher\Event;
+
+final class BeforeDeleteUserEvent extends Event
+{
+
+    public function __construct(private readonly User $user)
+    {
+    }
+
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+}
