e1daee14239bb011fca9df6cb4c3c3a3ae549823
enjoys <deadly.info@gmail.com>
2023-09-01 23:59:25 +0300

    fix: PHP Deprecated:  Creation of dynamic property

--- src/Modules/Module.php
+++ src/Modules/Module.php
@@ -2,35 +2,29 @@
 
 namespace EnjoysCMS\Core\Modules;
 
+use stdClass;
+
 class Module
 {
     public string $installPath;
     public string $path;
-    public ?string $routePath;
     public string $packageName;
-    public string $description;
+    public ?string $description = null;
     public array $paths;
     public string $moduleName;
     public array $adminLinks = [];
     public array $namespaces = [];
+    public ?stdClass $extra = null;
 
     public function __construct(object $data)
     {
-        foreach ($data as $k => $v) {
-            $this->$k = $v;
-        }
-        $this->path = $this->installPath;
-        $this->moduleName = $this->packageName;
-        $this->routePath = $this->getRoutePath();
-
-        if (isset($this->extra->{'module-name'})) {
-            $this->moduleName =  $this->extra->{'module-name'};
-        }
-        if (isset($this->extra->{'admin-links'})) {
-            $this->adminLinks =  (array)$this->extra->{'admin-links'};
-        }
+        $this->installPath = $this->path = $data->installPath;
+        $this->packageName = $data->packageName;
+        $this->extra = $data->extra;
+        $this->paths = $data->paths;
+        $this->moduleName = $this->extra?->{'module-name'} ?? $this->packageName;
+        $this->adminLinks = (array)($this->extra?->{'admin-links'} ?? null);
     }
-
 
 
     private function getRoutePath(): ?string
