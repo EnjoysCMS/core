7acf3de75b16d73218db6d1d73b300f58c0b16db
enjoys <deadly.info@gmail.com>
2023-09-02 00:23:40 +0300

    rename PrintModuleNameCommand.php to PrintSectionCommand.php

--- src/Console/Command/PrintSectionCommand.php
+++ src/Console/Command/PrintSectionCommand.php
@@ -8,7 +8,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class PrintModuleNameCommand extends Command
+class PrintSectionCommand extends Command
 {
 
     protected function configure(): void
