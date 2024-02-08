8b351e5f1a367fdc44e0d2e14c0522c02556427b
enjoys <deadly.info@gmail.com>
2024-02-08 08:49:37 +0300

    Exception: Class "Symfony\Component\Routing\Loader\AnnotationClassLoader" not found

--- src/Routing/AnnotationClassLoader.php
+++ src/Routing/AnnotationClassLoader.php
@@ -11,41 +11,48 @@ use Symfony\Component\Routing\Loader\AnnotationClassLoader as BaseLoader;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 
-/**
- * @deprecated since Symfony 6.4, to be removed in 7.0, use {@link AttributeClassLoader} instead
- */
-class AnnotationClassLoader extends BaseLoader
-{
-    protected function configureRoute(Route $route, ReflectionClass $class, ReflectionMethod $method, $annot): void
+
+if (class_exists(BaseLoader::class)) {
+    /**
+     * @deprecated since Symfony 6.4, to be removed in 7.0, use {@link AttributeClassLoader} instead
+     */
+    class AnnotationClassLoader extends BaseLoader
     {
-        if ('__invoke' === $method->getName()) {
-            $route->setDefault('_controller', $class->getName());
-        } else {
-            $route->setDefault('_controller', [$class->getName(), $method->getName()]);
-        }
-    }
-
-    protected function addRoute(
-        RouteCollection $collection,
-        object $annot,
-        array $globals,
-        \ReflectionClass $class,
-        \ReflectionMethod $method
-    ): void {
-        parent::addRoute($collection, $annot, $globals, $class, $method);
-
-        $name = $globals['name'].($annot->getName() ?? $this->getDefaultRouteName($class, $method));
-        $route = $collection->get($name);
-
-        if ($route === null){
-            return;
+        protected function configureRoute(Route $route, ReflectionClass $class, ReflectionMethod $method, $annot): void
+        {
+            if ('__invoke' === $method->getName()) {
+                $route->setDefault('_controller', $class->getName());
+            } else {
+                $route->setDefault('_controller', [$class->getName(), $method->getName()]);
+            }
         }
 
-        $options = $route->getOptions() ?? [];
-        $options['middlewares'] = array_merge($globals['options']['middlewares'] ?? [], $annot->getOptions()['middlewares'] ?? []);
-        $options['groups'] = array_merge($globals['options']['groups'] ?? [], $annot->getOptions()['groups'] ?? []);
-        $options['acl'] = (array_key_exists('acl', $options)) ? $options['acl'] : true;
+        protected function addRoute(
+            RouteCollection $collection,
+            object $annot,
+            array $globals,
+            ReflectionClass $class,
+            ReflectionMethod $method
+        ): void {
+            parent::addRoute($collection, $annot, $globals, $class, $method);
 
-        $route->setOptions($options);
+            $name = $globals['name'] . ($annot->getName() ?? $this->getDefaultRouteName($class, $method));
+            $route = $collection->get($name);
+
+            if ($route === null) {
+                return;
+            }
+
+            $options = $route->getOptions() ?? [];
+            $options['middlewares'] = array_merge(
+                $globals['options']['middlewares'] ?? [],
+                $annot->getOptions()['middlewares'] ?? []
+            );
+            $options['groups'] = array_merge($globals['options']['groups'] ?? [], $annot->getOptions()['groups'] ?? []);
+            $options['acl'] = (array_key_exists('acl', $options)) ? $options['acl'] : true;
+
+            $route->setOptions($options);
+        }
     }
 }
+
