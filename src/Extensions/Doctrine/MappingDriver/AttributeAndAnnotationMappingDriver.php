86289c39af09d240aa1b8d014eceef171ff95c09
enjoys <deadly.info@gmail.com>
2024-07-12 00:26:07 +0300

    fix load AnnotationDriver if exist

--- src/Extensions/Doctrine/MappingDriver/AttributeAndAnnotationMappingDriver.php
+++ src/Extensions/Doctrine/MappingDriver/AttributeAndAnnotationMappingDriver.php
@@ -13,69 +13,71 @@ use Doctrine\ORM\Mapping\MappingException;
 use Doctrine\Persistence\Mapping\ClassMetadata;
 use Psr\Cache\CacheItemPoolInterface;
 
-/**
- * [stackoverflow] Doctrine ORM 2.9 use both AnnotationDriver and AttributeDriver to parse entity metadata
- * @see https://stackoverflow.com/questions/68195822/doctrine-orm-2-9-use-both-annotationdriver-and-attributedriver-to-parse-entity-m
- * @deprecated Remove in 7.x., use AttributeDriver, because in Doctrine ORM 3.0 will not work AnnotationDriver
- */
-class AttributeAndAnnotationMappingDriver extends CompatibilityAnnotationDriver
-{
-    private AttributeDriver $attributeDriver;
-    private AnnotationDriver $annotationDriver;
-
-    public function __construct(
-        array $paths = [],
-        ?CacheItemPoolInterface $cache = null,
-        bool $reportFieldsWhereDeclared = true
-    ) {
-        @trigger_error('Because in Doctrine ORM 3.0 will not work AnnotationDriver', E_USER_DEPRECATED);
-
-        $this->attributeDriver = new AttributeDriver($paths, $reportFieldsWhereDeclared);
-
-
-        $reader = new AnnotationReader();
-        if ($cache !== null) {
-            $reader = new PsrCachedReader($reader, $cache);
-        }
-        $this->annotationDriver = new AnnotationDriver($reader, $paths, $reportFieldsWhereDeclared);
-    }
-
+if (class_exists(AnnotationDriver::class)) {
     /**
-     * @throws MappingException
+     * [stackoverflow] Doctrine ORM 2.9 use both AnnotationDriver and AttributeDriver to parse entity metadata
+     * @see https://stackoverflow.com/questions/68195822/doctrine-orm-2-9-use-both-annotationdriver-and-attributedriver-to-parse-entity-m
+     * @deprecated Remove in 7.x., use AttributeDriver, because in Doctrine ORM 3.0 will not work AnnotationDriver
      */
-    public function loadMetadataForClass(string $className, ClassMetadata $metadata): void
+    class AttributeAndAnnotationMappingDriver extends CompatibilityAnnotationDriver
     {
-        try {
-            $this->attributeDriver->loadMetadataForClass($className, $metadata);
-            return;
-        } catch (MappingException $e) {
-            if (!preg_match('/^Class(.)*$/', $e->getMessage())) {
-                throw $e;
+        private AttributeDriver $attributeDriver;
+        private AnnotationDriver $annotationDriver;
+
+        public function __construct(
+            array $paths = [],
+            ?CacheItemPoolInterface $cache = null,
+            bool $reportFieldsWhereDeclared = true
+        ) {
+            @trigger_error('Because in Doctrine ORM 3.0 will not work AnnotationDriver', E_USER_DEPRECATED);
+
+            $this->attributeDriver = new AttributeDriver($paths, $reportFieldsWhereDeclared);
+
+
+            $reader = new AnnotationReader();
+            if ($cache !== null) {
+                $reader = new PsrCachedReader($reader, $cache);
             }
+            $this->annotationDriver = new AnnotationDriver($reader, $paths, $reportFieldsWhereDeclared);
         }
-        $this->annotationDriver->loadMetadataForClass($className, $metadata);
-    }
 
-
-    /**
-     * @throws \Doctrine\Persistence\Mapping\MappingException
-     */
-    public function isTransient($className): bool
-    {
-        if (in_array($className, $this->attributeDriver->getAllClassNames(), true)) {
-            return $this->attributeDriver->isTransient($className);
+        /**
+         * @throws MappingException
+         */
+        public function loadMetadataForClass(string $className, ClassMetadata $metadata): void
+        {
+            try {
+                $this->attributeDriver->loadMetadataForClass($className, $metadata);
+                return;
+            } catch (MappingException $e) {
+                if (!preg_match('/^Class(.)*$/', $e->getMessage())) {
+                    throw $e;
+                }
+            }
+            $this->annotationDriver->loadMetadataForClass($className, $metadata);
         }
-        return $this->annotationDriver->isTransient($className);
-    }
 
-    /**
-     * @throws \Doctrine\Persistence\Mapping\MappingException
-     */
-    public function getAllClassNames(): array
-    {
-        return array_merge(
-            $this->annotationDriver->getAllClassNames(),
-            $this->attributeDriver->getAllClassNames()
-        );
+
+        /**
+         * @throws \Doctrine\Persistence\Mapping\MappingException
+         */
+        public function isTransient($className): bool
+        {
+            if (in_array($className, $this->attributeDriver->getAllClassNames(), true)) {
+                return $this->attributeDriver->isTransient($className);
+            }
+            return $this->annotationDriver->isTransient($className);
+        }
+
+        /**
+         * @throws \Doctrine\Persistence\Mapping\MappingException
+         */
+        public function getAllClassNames(): array
+        {
+            return array_merge(
+                $this->annotationDriver->getAllClassNames(),
+                $this->attributeDriver->getAllClassNames()
+            );
+        }
     }
 }
