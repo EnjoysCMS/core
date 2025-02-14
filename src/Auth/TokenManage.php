c27e0ee6ee54a137375f806f049537164db51604
enjoys <deadly.info@gmail.com>
2025-02-14 08:45:55 +0300

    fix major bug: upstream sent too big header while reading response header from upstream

--- src/Auth/TokenManage.php
+++ src/Auth/TokenManage.php
@@ -87,13 +87,18 @@ class TokenManage
     public function delete(): void
     {
         $token = $this->cookie->get($this->tokenName);
-        $this->cookie->delete($this->tokenName);
 
-        if (!Uuid::isValid($token ?? '')){
+        if ($token === null) {
             return;
         }
 
-        $tokenEntity = $this->repository->find($token ?? '');
+        $this->cookie->delete($this->tokenName);
+
+        if (!Uuid::isValid($token)){
+            return;
+        }
+
+        $tokenEntity = $this->repository->find($token);
         if ($tokenEntity === null) {
             return;
         }
