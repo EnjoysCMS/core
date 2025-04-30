8a7bf93815f16dfb227af2bbd0ed3b85e3c71cc8
enjoys <deadly.info@gmail.com>
2025-05-01 00:09:35 +0300

    fix: Error: Interface "Composer\Plugin\PluginInterface" not found

--- src/Block/Loader/BlockLoader.php
+++ src/Block/Loader/BlockLoader.php
@@ -8,6 +8,7 @@ use Doctrine\Common\Annotations\Reader;
 use EnjoysCMS\Core\Block\AbstractBlock;
 use EnjoysCMS\Core\Block\Annotation\Block;
 use EnjoysCMS\Core\Block\Collection;
+use Error;
 use ReflectionClass;
 use ReflectionException;
 use Symfony\Component\Finder\Finder;
@@ -23,35 +24,32 @@ class BlockLoader extends AnnotationLoader
     }
 
 
-    /**
-     * @throws ReflectionException
-     */
     public function getCollection(): Collection
     {
         $collection = new Collection();
 
         foreach ($this->finder as $file) {
-            try {
-                /** @var class-string $class */
-                if ($class = $this->findClass($file->getPathname())) {
+            /** @var class-string $class */
+            if ($class = $this->findClass($file->getPathname())) {
+                try {
                     $reflectionClass = new ReflectionClass($class);
-
-                    if ($reflectionClass->isAbstract()) {
-                        continue;
-                    }
-
-                    if (!$reflectionClass->isSubclassOf(AbstractBlock::class)
-                    ) {
-                        continue;
-                    }
-
-                    foreach ($this->getAnnotations($reflectionClass) as $annotation) {
-                        $annotation->setReflectionClass($reflectionClass);
-                        $collection->addAnnotation($annotation);
-                    }
+                } catch (ReflectionException|Error) {
+                    continue;
                 }
-            } catch (ReflectionException) {
-                //...
+
+                if ($reflectionClass->isAbstract()) {
+                    continue;
+                }
+
+                if (!$reflectionClass->isSubclassOf(AbstractBlock::class)
+                ) {
+                    continue;
+                }
+
+                foreach ($this->getAnnotations($reflectionClass) as $annotation) {
+                    $annotation->setReflectionClass($reflectionClass);
+                    $collection->addAnnotation($annotation);
+                }
             }
         }
 
